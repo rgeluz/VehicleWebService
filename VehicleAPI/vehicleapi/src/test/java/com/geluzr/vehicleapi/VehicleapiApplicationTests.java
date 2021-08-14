f1af8a104df82ab3054b5756a03abcc3ed471096
Roman <rgeluz22@yahoo.com>
2021-08-14 11:59:39 -0700

    added initial Spring Boot project generated by Spring Initializer

---
+++ VehicleAPI/vehicleapi/src/test/java/com/geluzr/vehicleapi/VehicleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geluzr.vehicleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
